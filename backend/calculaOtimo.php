65715452160bf7dc6f351cbbae2aa0a0193522a0
Arthur Soares <arthurvsp97@gmail.com>
2018-05-09 13:38:39 -0300

    Correção tabela, arganização de arquivos

--- backend/calculaOtimo.php
+++ backend/calculaOtimo.php
@@ -35,31 +35,17 @@
         $tamanhoBarra = $_POST["tamanhoBarra"];
         $demandas = [];
 
-        echo "Contador: ".$_POST["contador"]."\n";
-
         for ($i = 0; $i <= (int)$_POST["contador"]; $i++)
         {
-            echo "i: ".$i."\n";
-            echo "Tamanho corte: ".$_POST["tamanhoCorte".$i]."\n";
-            echo "quantidade corte: ".$_POST["quantidadeCorte".$i]."\n\n";
             array_push($demandas, new Demanda($_POST["tamanhoCorte".$i], $_POST["quantidadeCorte".$i]));
         }
-        // $demandas[0] = new Demanda(300, 45);
-        // $demandas[1] = new Demanda(150, 23);
-        // $demandas[2] = new Demanda(120, 16);
-        // $demandas[3] = new Demanda(980, 5);
-
-        // echo "Tamanho da barra ".$tamanhoBarra."<br/>";
-        // echo "Demanda ".$demandas[0]->tamanho."<br/>";
-        // echo "Quantidade ".$demandas[0]->quantidade."<br/>";
-        // echo "Size of ".sizeof($demandas)."<br/>";
 
         gravarDados($arquivo, $tamanhoBarra, $demandas);
 
-        echo shell_exec("./cspsol --data dadosCliente.txt");
+        shell_exec("./cspsol --data dadosCliente.txt");
 
         echo "<script>console.log('Otimização calculada');</script>";
-        echo "<script>window.open('../frontend/index.html');</script>";
+        echo "<script>window.location.replace('../frontend/resultado.html');</script>";
 
     ?>
  </body>
